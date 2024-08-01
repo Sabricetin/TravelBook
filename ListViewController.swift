553d881a1fd70dce6b7dfb978b1ee064ca43bf21
Sabricetin <sabriyazilim@gmail.com>
2024-08-01 15:39:13 +0300

    The application has been updated, updates have been made in the design area and a logo has been added to the application,and constraint was added

--- ListViewController.swift
+++ ListViewController.swift
@@ -6,24 +6,104 @@
 //
 
 import UIKit
+import CoreData
 
-class ListViewController: UIViewController {
+class ListViewController: UIViewController , UITableViewDelegate , UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    var tittleArray = [String]()
+    var idArray = [UUID]()
+    var chosenTitle = ""
+    var chosenİd : UUID?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        getData()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+           NotificationCenter.default.addObserver(self, selector: #selector(getData), name: NSNotification.Name("newPlace"), object: nil)
+       }
+    
+    //Verileri TableView'de Göstermek
+    
+   @objc func getData () {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Places")
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try context.fetch(request)
+            
+            if results.count > 0 {
+                self.tittleArray.removeAll(keepingCapacity: false)
+                self.idArray.removeAll(keepingCapacity: false)
+                
+                for result in results   as! [NSManagedObject] {
+                    if let tittle =  result.value(forKey: "tittle") as? String {
+                        self.tittleArray.append(tittle)
+                    }
+                    if let id = result.value(forKey: "id") as? UUID {
+                        self.idArray.append(id)
+                    }
+                    tableView.reloadData()
+                }
+            }
+            
+        }catch {
+            print ("error!!!")
+        }
     }
-    */
-
+    
+    //Diğer Sayfaya Gitme
+    
+    @objc func addButtonClicked () {
+        chosenTitle = ""
+        performSegue(withIdentifier: "toViewController", sender: nil)    }
+    
+    //TableView Ayarları
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return tittleArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        var content = cell.defaultContentConfiguration()
+        content.text = tittleArray[indexPath.row]
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    // Seçilen Veriyi Yollamak
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        chosenTitle = tittleArray[indexPath.row]
+        chosenİd = idArray[indexPath.row]
+        
+        performSegue(withIdentifier: "toViewController", sender: nil )
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toViewController" {
+            let destinationVC = segue.destination as! ViewController
+            destinationVC.selectedTitle = chosenTitle
+            destinationVC.selectedİd = chosenİd
+            
+        }
+    }
 }
--- TravelBook/ViewController.swift
+++ TravelBook/ViewController.swift
@@ -4,16 +4,206 @@
 //
 //  Created by Sabri Çetin on 16.05.2024.
 //
-
 import UIKit
+import MapKit
+import CoreLocation
+import CoreData
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , MKMapViewDelegate , CLLocationManagerDelegate {
+    
+    @IBOutlet weak var nameTxt: UITextField!
+    @IBOutlet weak var commendTxt: UITextField!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    var chosenLatitude = Double()
+    var chosenLongitude = Double()
+    
+    var selectedTitle = ""
+    var selectedİd : UUID?
+    
+    var annatationTitle = ""
+    var annatationSubtitle = ""
+    var anatationLatitude = Double()
+    var anatationLongitude = Double()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        //Kullanıcının Yerini Tespit Etme
+        
+        mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        // Kullanıcı Yerini Haritada İşaretleyebilme
+        
+        let gesturRecogizer =  UILongPressGestureRecognizer(target: self, action: #selector(chooseLocation(gesturRecognizer:)))
+        
+        gesturRecogizer.minimumPressDuration = 2
+        mapView.addGestureRecognizer( gesturRecogizer)
+        
+        if selectedTitle != "" {
+            
+            //CoreData
+            
+            //Seçilen Konumu Haritada Gösterme
+            let appDelgete = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelgete.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Places")
+            let idString = selectedİd!.uuidString
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idString)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do {
+                let result = try context.fetch(fetchRequest)
+                if result.count > 0 {
+                    for result in result as! [NSManagedObject]{
+                        
+                    if let title = result.value(forKey: "tittle") as? String {
+                        annatationTitle = title
+                        
+                        if let subtitle = result.value(forKey: "subtitle") as? String {
+                            annatationSubtitle = subtitle
+                            
+                            if let latitude = result.value(forKey: "latitude") as? Double {
+                                anatationLatitude = latitude
+                                
+                                if let longitude = result.value(forKey: "longitude") as? Double {
+                                    anatationLongitude = longitude
+                        
+                        let annatation = MKPointAnnotation()
+                        annatation.title = annatationTitle
+                        annatation.subtitle = annatationSubtitle
+                        let coordinate = CLLocationCoordinate2D(latitude: anatationLatitude, longitude: anatationLongitude)
+                        
+                        annatation.coordinate = coordinate
+                        
+                        mapView.addAnnotation(annatation)
+                        nameTxt.text = annatationTitle
+                        commendTxt.text = annatationSubtitle
+                                    
+                        locationManager.stopUpdatingLocation()
+                                    
+                        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+                        let region = MKCoordinateRegion(center: coordinate, span: span)
+                        mapView.setRegion(region, animated: true)
+                                }
+                            }
+                        }
+                    }
+                }
+                }
+            }
+            catch {
+                print ("error!!!")
+            }
+        } else {
+            //Add New Data
+        }
     }
-
-
+    
+    @objc func chooseLocation (gesturRecognizer:UILongPressGestureRecognizer) {
+        
+        if gesturRecognizer.state == .began {
+            
+            let touchPoint = gesturRecognizer.location(in: self.mapView)
+            let touchCordinate = self.mapView.convert(touchPoint, toCoordinateFrom: self.mapView)
+            
+            chosenLatitude = touchCordinate.latitude
+            chosenLongitude = touchCordinate.longitude
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = touchCordinate
+            annotation.title = nameTxt.text
+            annotation.subtitle = commendTxt.text
+            self.mapView.addAnnotation(annotation)
+        }
+    }
+        //Kullanıcının Yerini Haritada Zoomlama
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
+        locationManager.stopUpdatingLocation()
+        if selectedTitle == "" {
+            let location = CLLocationCoordinate2D(latitude:  locations [0].coordinate.latitude , longitude: locations[0].coordinate.longitude)
+            let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+            let region = MKCoordinateRegion(center: location, span: span)
+            mapView.setRegion(region, animated: true) }
+        else {
+            //
+        }
+    }
+    //Annatation Özelleşitrme Pini Özelleşitrme
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            
+            if annotation is MKUserLocation {
+                return nil
+            }
+            let reuseId = "myAnnotation"
+            var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKMarkerAnnotationView
+            
+            if pinView == nil {
+                pinView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+                pinView?.canShowCallout = true
+                pinView?.tintColor = UIColor.blue
+                
+                let button = UIButton(type: UIButton.ButtonType.detailDisclosure)
+                pinView?.rightCalloutAccessoryView = button
+                
+            } else {
+                pinView?.annotation = annotation
+            }
+            return pinView
+        }
+    //Navigasyonu Açma
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if selectedTitle != "" {
+            
+            let requestLocation = CLLocation(latitude: anatationLatitude, longitude: anatationLongitude)
+            
+            CLGeocoder().reverseGeocodeLocation(requestLocation) { (placemarks , error) in
+            
+                if let placemark = placemarks {
+                    if placemark.count > 0 {
+                        let newPlacemark = MKPlacemark(placemark: placemark[0])
+                        let item = MKMapItem(placemark: newPlacemark)
+                        item.name = self.annatationTitle
+                        let launchOptions = [MKLaunchOptionsDirectionsModeKey:MKLaunchOptionsDirectionsModeDriving]
+                        item.openInMaps(launchOptions: launchOptions)
+                    }
+                }
+            }
+        }
+    }
+    // Konumu Haritada Üzerinde Girilen Texti Gösterip Kaydetme
+    @IBAction func saveButton(_ sender: Any) {
+        
+        let appDelegete = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegete.persistentContainer.viewContext
+        let newPlace = NSEntityDescription.insertNewObject(forEntityName: "Places", into: context)
+        
+        newPlace.setValue(nameTxt.text, forKey: "tittle")
+        newPlace.setValue(commendTxt.text, forKey: "subtitle")
+        newPlace.setValue( chosenLatitude, forKey: "latitude")
+        newPlace.setValue(chosenLongitude, forKey: "longitude")
+        newPlace.setValue(UUID(), forKey: "id")
+        
+        do {
+            try context.save()
+            print ("Success!!!")
+        } catch{
+            print ("Error!!!")
+        }
+        
+        NotificationCenter.default.post(name: NSNotification.Name("newPlace"), object: nil)
+        navigationController?.popViewController(animated: true)
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            self.view.endEditing(true)
+        }
 }
-
